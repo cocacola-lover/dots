ef478d2aed24eecd32741e8cae170d28ad33dbce
Roman Ivashkin <dev@ivashk.ru>
2026-01-16 13:51:26 +0300

    ADD: update fedora config

--- setup_fedora.sh
+++ setup_fedora.sh
@@ -25,16 +25,31 @@ if [ "$stage" -eq 2 ]; then
     sudo dnf config-manager addrepo --from-repofile=https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
     sudo dnf install brave-browser
 
+    echo "Setup Flatpak"
+    flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
+    echo "Install Flatpack Main Apps"
+    flatpak install flathub org.telegram.desktop org.mozilla.Thunderbird com.getpostman.Postman
+    echo "Details to configure Thunderbird: https://timeweb.cloud/docs/mail/configuring-email-clients/thunderbird" 
+
+    echo "Install dnf Main Apps"
+    sudo dnf copr enable dejan/lazygit
+    sudo dnf install libreoffice vlc lazygit
+
     echo "Install Dev Utils"
-    sudo dnf install git nvim
+    sudo dnf install git nvim kitty fzf ripgrep fd 
+    npm install -g tree-sitter-cli
+    # If get access error look into https://docs.npmjs.com/resolving-eacces-permissions-errors-when-installing-packages-globally
 
     echo "Setup Git"
     git config --global user.email "dev@ivashk.ru"
     git config --global user.name "Roman Ivashkin"
     git config --global core.editor nvim
     git config --global init.defaultbranch main
-
+if [ "$stage" -eq 3 ]; then
+    echo "Clean up"
+    sudo remove firefox foot
 else
-    echo "Error: Invalid option. Please use 1 or 2"
+    echo "Error: Invalid option. Please use 1, 2 or 3"
     exit 1
 fi
